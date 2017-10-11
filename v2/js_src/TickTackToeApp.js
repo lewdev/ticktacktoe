f376507c3dc4f20ff7b84d039af9eaa8618f3410
Owner <Owner@hataraku>
2017-10-10 20:38:49 -1000

    Initial commit.

---
+++ v2/js_src/TickTackToeApp.js
@@ -0,0 +1,16 @@
+
+/**
+ * This is called back by the Google Maps script when it is finished initializing.
+ */
+window.initialize = function() {
+
+};
+
+function TickTackToeApp() {
+  var thisRef = this;
+  /**
+   * @private
+   * @type {TickTackBlocks}
+   */
+  thisRef.blockArray = [];
+}
